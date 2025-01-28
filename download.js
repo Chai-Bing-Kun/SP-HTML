bd1c2f0ae710eeb1860a00dad03a6593bae02bbf
初 <153291568+Chai-Bing-Kun@users.noreply.github.com>
2025-01-28 12:28:25 +0800

    Add files via upload

--- download.js
+++ download.js
@@ -15,25 +15,35 @@ function download(type) {
     let files;
     if (currentProduct === 'spark5') {
         files = {
-            'apk': 'https://github.com/Chai-Bing-Kun/Spark-X/releases/download/v.5.3.2.5/H53034E71__20250123093440.apk',
-            'windows': 'https://github.com/Chai-Bing-Kun/Spark-X/releases/download/v.5.3.2.5/Spark.5.3.2.5.zip',
-            'mac': 'https://github.com/Chai-Bing-Kun/Spark-X/releases/download/v.5.3.2.5/Spark.5.3.2.5.WKWebView.macOS.zip',
-            'linux': 'https://github.com/Chai-Bing-Kun/Spark-X/releases/download/v.5.3.2.5/Spark.5.3.2.5.64.Electron.Linux.zip'
+            'apk': 'https://github.com/Chai-Bing-Kun/Spark-X/releases/download/v.5.3.2.6/H53034E71__20250127215051.apk',
+            'windows': 'https://github.com/Chai-Bing-Kun/Spark-X/releases/download/v.5.3.2.6/Spark.5.3.2.6.zip',
+            'mac': 'https://github.com/Chai-Bing-Kun/Spark-X/releases/download/v.5.3.2.6/Spark.5.3.2.6.WKWebView.macOS.zip',
+            'linux': 'https://github.com/Chai-Bing-Kun/Spark-X/releases/download/v.5.3.2.6/Spark.5.3.2.6.64.Electron.Linux.zip',
+            'fastapk': 'https://ghproxy.cn/https://github.com/Chai-Bing-Kun/Spark-X/releases/download/v.5.3.2.6/H53034E71__20250127215051.apk',
+            'fastwindows': 'https://ghproxy.cn/https://github.com/Chai-Bing-Kun/Spark-X/releases/download/v.5.3.2.6/Spark.5.3.2.6.zip',
+            'fastmac': 'https://ghproxy.cn/https://github.com/Chai-Bing-Kun/Spark-X/releases/download/v.5.3.2.6/Spark.5.3.2.6.WKWebView.macOS.zip',
+            'fastlinux': 'https://ghproxy.cn/https://github.com/Chai-Bing-Kun/Spark-X/releases/download/v.5.3.2.6/Spark.5.3.2.6.64.Electron.Linux.zip'
         };
     } else if (currentProduct === 'spark4') {
         files = {
             'apk': 'https://github.com/Chai-Bing-Kun/Spark-X/releases/download/v.4.1.1.0/H5DD2E11E__20240727081549.apk',
             'windows': 'download/致歉声明.txt',
             'mac': 'download/致歉声明.txt',
-            'linux': 'download/致歉声明.txt'
+            'linux': 'download/致歉声明.txt',
+            'fastapk': 'https://ghproxy.cn/https://github.com/Chai-Bing-Kun/Spark-X/releases/download/v.4.1.1.0/H5DD2E11E__20240727081549.apk',
+            'fastwindows': 'download/致歉声明.txt',
+            'fastmac': 'download/致歉声明.txt',
+            'fastlinux': 'download/致歉声明.txt'
         };
     }
 
     if (type === 'all') {
         if (currentProduct === 'spark5') {
-            window.open('https://github.com/Chai-Bing-Kun/Spark-X/archive/refs/tags/v.5.2.1.1.tar.gz', '_blank');
+            // Spark 5 的全部下载链接
+            window.open('https://ghproxy.cn/https://github.com/Chai-Bing-Kun/Spark-X/archive/refs/tags/v.5.3.2.6.zip', '_blank');
         } else if (currentProduct === 'spark4') {
-            window.open(files['apk'], '_blank');
+            // Spark 4 的全部下载链接
+            window.open('https://ghproxy.cn/https://github.com/Chai-Bing-Kun/Spark-X/archive/refs/tags/v.4.1.1.0.zip', '_blank');
         }
     } else {
         window.open(files[type], '_blank');
